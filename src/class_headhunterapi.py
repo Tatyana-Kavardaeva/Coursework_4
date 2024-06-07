75ecdf0cfc2751366d564267b066f10d81c96e4a
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-06-07 12:54:01 +0300

    update get_vacancies class_headhunterapi.py

--- src/class_headhunterapi.py
+++ src/class_headhunterapi.py
@@ -12,9 +12,13 @@ class HeadHunterAPI(AbstractAPI):
 
     def get_vacancies(self, keyword):
         self.params['text'] = keyword
-        while self.params['page'] != 20:
-            response = requests.get(self.url, self.params)
-            vacancies = response.json()['items']
+        while True:
+            # self.params['page'] = page
+            response = requests.get(self.url, params=self.params)
+            data = response.json()
+            vacancies = data.get('items', [])
+            if not vacancies:
+                break
             self.vacancies.extend(vacancies)
             self.params['page'] += 1
         return self.vacancies
@@ -23,6 +27,6 @@ class HeadHunterAPI(AbstractAPI):
 if __name__ == "__main__":
     hh_api = HeadHunterAPI("https://api.hh.ru/vacancies")
     l_h = hh_api.get_vacancies("Python")
-    # print(l_h)
-    for l in l_h:
-        print(l.get('salary'))
+    print(len(l_h))
+    # for l in l_h:
+    #     print(l.get('salary'))
